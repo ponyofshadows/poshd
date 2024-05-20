22a0b15f4bbc11ce9b705c87034238f19d85dfd3
lipidraft <absolutehollow0@gmail.com>
2024-05-20 15:21:16 +0800

    flow control

---
+++ lib/functions.sh
@@ -0,0 +1,21 @@
+#
+## functions.sh
+#
+
+po_init(){
+   mkdir -p $PO_PATH/list
+   mkdir -p $PO_PATH/proj
+   mkdir -p $PO_PATH/.hidden_proj
+   mkdir -p $PO_PATH/.tmp
+   echo "The path to archive files is '$PO_PATH'"
+}
+
+po_date(){
+
+}
+
+po_period(){
+
+}
+
+
