786c8458e670a23d1d5e79710521b35ffcccfd9d
lipidraft <absolutehollow0@gmail.com>
2024-06-02 12:44:57 +0800

    test: po -p

--- test/proj.sh
+++ test/proj.sh
@@ -2,7 +2,37 @@
 # test: po -p
 ./po --init
 echo "[test] add a new project and a new hidden project"
-./po --debug -p master plan -p:hide love
+./po --debug -p:active master plan -p:hide love
 echo "[test] add a project with floder"
 ./po --debug -p play/screenshots
-echo ""
+echo "[test] active a hidden project; move file into two projects"
+touch /tmp/test/s1.png
+touch /tmp/test/s2.png
+echo "------------tree before-----------------"
+tree -a /tmp/test
+echo "----------------------------------------"
+./po --debug -f "/tmp/test/s*.png" -p:active love/images/screenshots -p "play/s*s"
+echo "------------tree after-----------------"
+tree -a /tmp/test
+echo "----------------------------------------"
+echo "[test] hide a project; remove a project"
+./po --debug -p:hide master plan -p --rm love 
+echo "------------tree-----------------"
+tree -a /tmp/test
+echo "---------------------------------"
+echo "[test] link project files to event"
+./po --debug -p:file "play/screenshots/*" -e play screenshots
+echo "------------tree-----------------"
+tree -a /tmp/test
+echo "---------------------------------"
+echo "[test] mv a file into projects and events"
+touch /tmp/test/info.yaml
+./po --debug -f /tmp/test/info.yaml -e play screenshots/doc -p play/doc -p master plan/doc
+echo "------------tree-----------------"
+tree -a /tmp/test
+echo "---------------------------------"
+
+echo "[test] done"
+rm -rf  /tmp/test
+
+
