b4f1296d228ce5adb0740fdf5b2efe94ccc5ebf5
lipidraft <absolutehollow0@gmail.com>
2024-06-10 19:57:53 +0800

    bash

--- po.sh
+++ po.sh
@@ -7,9 +7,172 @@
 PO_PATH="${HOME}/all"
 mkdir -p "$PO_PATH"
 
-. ${PO_PATH}/proj/sw/bash_tools/poshd/lib/object.sh
-. ${PO_PATH}/proj/sw/bash_tools/poshd/lib/time.sh
+#
+## object func
+#
+po_complete_object(){
+  if [[ -n "$current_content" ]]; then
+    if [[ "${current_object["type"]}" == "event" || "${current_object["type"]}" == "project" ]]; then
+      # title
+      current_object["content"]="${current_content%%/*}"
+      # path
+      if [[ "$current_content" == */* ]]; then
+        current_object["path"]="${current_content#*/}"
+      fi
+    else
+      current_object["content"]="$current_content"
+    fi
+  fi
+  if [[ (-n "$current_content") || "${current_object[type]}" == "keywords" ]]; then
+    current_content=""
+    local object_count=${#object_key_list[@]}
+    # DEBUG{
+    #if [[ -n PO_DEBUG ]]; then
+      #echo "[DEBUG](po) *** A New Object ***"
+      #echo "[DEBUG](po) type: ${current_object["type"]}"
+      #echo "[DEBUG](po) key: ${!current_object[@]}"
+      #echo "[DEBUG](po) value: ${current_object[@]}"
+      #echo "[DEBUG](po) object_count=$object_count"
+    #fi
+    # }DEBUG
+    object_key_list[object_count]="${!current_object[@]}"
+    object_value_list[object_count]="${current_object[@]}"
+    unset current_object
+    declare -g -A current_object
+    current_object["type"]="event"
+  fi
+}
 
+#
+## time func
+#
+po_date(){
+  local formatted_time=$(date +'%y%m%d%H')
+  local time_code_queue=$1
+  local current_time_code=""
+  if [[ !( $time_code_queue == +* || $time_code_queue == -* ) ]]; then
+    time_code_queue="+$time_code_queue"
+  fi
+  # DEBUG{
+  #if [[ -n "$PO_DEBUG" ]]; then
+    #echo "[DEBUG](time.po_date) \$time_code_queue=$time_code_queue" >&2
+  #fi
+  # }DEBUG
+  while [[ -n "$time_code_queue" ]]; do
+    if [[ $time_code_queue =~ ^([+-])(.*?)([+-].*|$) ]]; then
+      current_time_code="${BASH_REMATCH[1]}${BASH_REMATCH[2]}"
+      time_code_queue="${BASH_REMATCH[3]}"  
+    else
+      current_time_code="$string"
+      time_code_queue=""
+    fi
+    case "$current_time_code" in
+      ?now)
+        local formatted_now=$(date +'%y%m%d%H')
+        formatted_time=$formatted_now
+        ;;
+      ?td)
+        local formatted_td=$(date +'%y%m%d')
+        formatted_time="$formatted_td${formatted_time:6}"
+        ;;
+      ?ytd)
+        local formatted_ytd=$(date -d 'yesterday' +'%y%m%d')
+        formatted_time="$formatted_ytd${formatted_time:6}"
+        ;;
+      ?tmrw)
+        local formatted_tmrw=$(date -d 'tomorrow' +'%y%m%d')
+        formatted_time="$formatted_tmrw${formatted_time:6}"
+        ;;
+      ?em)
+        formatted_time="${formatted_time:0:6}03"
+        ;;
+      ?sr)
+        formatted_time="${formatted_time:0:6}06"
+        ;;
+      ?m)
+        formatted_time="${formatted_time:0:6}09"
+        ;;
+      ?noon)
+        formatted_time="${formatted_time:0:6}12"
+        ;;
+      ?a)
+        formatted_time="${formatted_time:0:6}15"
+        ;;
+      ?ss)
+        formatted_time="${formatted_time:0:6}18"
+        ;;
+      ?n|?twi)
+        formatted_time="${formatted_time:0:6}21"
+        ;;
+      ?mn)
+        formatted_time="${formatted_time:0:6}24"
+        ;;
+      [^0-9][0-9][0-9])
+        formatted_time="${formatted_time:0:6}${current_time_code:1}"
+        ;;
+      [^0-9][0-9][0-9][0-9][0-9])
+        formatted_time="${formatted_time:0:4}${current_time_code:1}"
+        ;;
+      [^0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+        formatted_time="${formatted_time:0:2}${current_time_code:1}"
+        ;;
+      [^0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+        formatted_time="${current_time_code:1}"
+        ;;
+      [+-][0-9]*[dh]*)
+        local change_day=$(echo "$current_time_code" | grep -oP '\d+(?=d)')
+        local change_hour=$(echo "$current_time_code" | grep -oP '\d+(?=h)')
+        local change_time_by_hour=$((change_day * 24 + change_hour)) 
+        if [[ "$current_time_code" == -* ]]; then
+          change_time_by_hour=$((- change_time_by_hour))
+        fi
+        formatted_time=$(date -u -d "20${formatted_time:0:2}-${formatted_time:2:2}-${formatted_time:4:2} ${formatted_time:6:2}:00 UTC + $change_time_by_hour hours" +"%y%m%d%H")
+        ;;
+    esac
+    # DEBUG{
+    #if [[ -n "$PO_DEBUG" ]]; then
+      #echo "[DEBUG](time.po_date) \$current_time_code=$current_time_code" >&2
+      #echo "[DEBUG](time.po_date) \$formatted_time=$formatted_time" >&2
+    #fi
+    # }DEUBG
+  done
+  echo "$formatted_time"
+}
+
+po_period(){
+  local current_time=$(po_date "")
+  local period
+  case "$1" in
+    H)
+      period="$current_time" 
+      ;;
+    d)
+      period="${current_time:0:6}"
+      ;;
+    ""|m)
+      period="${current_time:0:4}"
+      ;;
+    y)
+      period="${current_time:0:2}"
+      ;;
+    [0-9][0-9]|[0-9][0-9]0-9][0-9]|[0-9][0-9][0-9][0-9][0-9][0-9]|[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9])
+      period="$1"
+      ;;
+    *)
+      period=""
+      ;;
+  esac
+  local period_text_length=${#period}
+  while (( $period_text_length < 8 )); do
+    period="${period}[0-9]"
+    let period_text_length++
+  done
+  echo "$period"
+}
+
+#
+## main: po
+#
 po() {
   # ----------------
   ## Strategies for Parsing Parameters
